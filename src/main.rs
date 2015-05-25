5e21455bbe28cbaab85edad6ce22ded507e92ae8
Sebastian Green-Husted <ricecake@tfm.nu>
2015-05-24 21:54:53 -0400

    look! threads!

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 #![feature(lookup_host)]
+#![feature(ip_addr)]
+#![feature(scoped)]
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
-    for host in std::net::lookup_host("tfm.nu").unwrap() {
-        println!("found address: {}", host.unwrap());
-    }
+	println!("Hello, world!");
+	for host in std::net::lookup_host("tfm.nu").unwrap() {
+		let _ = thread::scoped(move || {
+			println!("found address: {}", host.unwrap().ip());
+		});
+	}
 }
